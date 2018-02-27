c220c6520278a0a9747729a3d67cee811a188acd
jaco <Lintveltjaco@gmail.com>
2018-02-27 13:19:23 +0200

    first push

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        ?>
+    </body>
+</html>
+
+<!--
+"1","Andre","van Zuydam", "A", "33","13/02/1979" 
+"2","Tyron James", "Hall", "TJ", "32", "03/06/1980";
+-->
