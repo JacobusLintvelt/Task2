2483d6b2ed2a741663468f480b50f1ed3f14673d
jaco <Lintveltjaco@gmail.com>
2018-02-27 16:37:36 +0200

    4 o commit

--- FunctionsPage.php
+++ FunctionsPage.php
@@ -1,8 +1,40 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function GenerateLine($index, $name, $surname) {
 
+    $dateOfBirth = GenDateOfBirth();
+    $age = Getage($dateOfBirth);
+    $initials = Getinitials($name);
+
+    $line = array($index, $name, Surname, $initials, $age, $dateOfBirth);
+}
+
+  function GenDateOfBirth() {
+
+    $day = rand(1, 30);
+    $month = rand(1, 12);
+    $year = rand(1970, date("Y"));
+
+    $date = $day . "-" . $month . "-" . $year;
+    $time = strtotime($date);
+    $newformat = date('d-m-Y',$time);
+    if (checkdate($month, $day, $year) == True) {
+        return $newformat;
+    } else {
+        return GenDateOfBirth();
+    }
+}
+
+function Getage($dateOfBirth)
+{
+   $year =  date('Y', $dateOfBirth );
+   
+   $age = date("Y") - $year;
+    
+    return $age;
+            
+}
+
+function GetInisials($name){
+    
+}
